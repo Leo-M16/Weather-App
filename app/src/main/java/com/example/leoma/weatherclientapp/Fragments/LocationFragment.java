02319f9e67b61f6abe5c4d001e4ffb837a18cc9a
Leo-M16 <33518508+Leo-M16@users.noreply.github.com>
2020-07-07 07:19:55 +0100

    Update

--- app/src/main/java/com/example/leoma/weatherclientapp/Fragments/LocationFragment.java
+++ app/src/main/java/com/example/leoma/weatherclientapp/Fragments/LocationFragment.java
@@ -96,7 +96,7 @@ public class LocationFragment extends Fragment {
 
     }
 
-
+/*Add some commentsss*/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
